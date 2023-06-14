80d65f4d3e7fb9994ff438b3a0bc9f492b233c7d
joffre <joffre.hermosilla@gmail.com>
2023-06-14 18:06:04 -0500

    chore[0] configuracion de cursos en webflux fase 1

---
+++ src/test/java/hackathon/nttdata/coderpath/cursos/CursosApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
