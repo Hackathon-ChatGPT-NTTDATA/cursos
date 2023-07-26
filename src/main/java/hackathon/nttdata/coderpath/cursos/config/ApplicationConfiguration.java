a35a8e76ba977cdc01bcf81a1c892a35da0659db
joffre <joffre.hermosilla@gmail.com>
2023-07-26 13:28:28 -0500

    chore[8] nueva configuracion para webclient

--- src/main/java/hackathon/nttdata/coderpath/cursos/config/ApplicationConfiguration.java
+++ src/main/java/hackathon/nttdata/coderpath/cursos/config/ApplicationConfiguration.java
@@ -2,25 +2,32 @@ package hackathon.nttdata.coderpath.cursos.config;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
-
 
 @Configuration
 @ConfigurationProperties
 @RefreshScope
 @Getter
 @Setter
-public class ApplicationConfiguration {
+public class ApplicationConfiguration extends org.springframework.web.cors.CorsConfiguration {
 
-    @Value("${config.balanceador.test}")
-    private String balanceadorTest;
-    
-    @Value("${config.base.endpoint}")
+	@Value("${config.balanceador.test}")
+	private String balanceadorTest;
+
+	@Value("${config.base.endpoint}")
 	private String urlExamenes;
 
 	@Bean
@@ -29,4 +36,25 @@ public class ApplicationConfiguration {
 		return WebClient.create(urlExamenes);
 
 	}
+
+	@Bean
+	public WebProperties.Resources resources() {
+		return new WebProperties.Resources();
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		corsConfig.addAllowedHeader("Content-Type");
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
+
 }
