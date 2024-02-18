ec17ad32ae0abb869b0a9912ca273de00ae190dd
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2024-02-17 23:44:27 -0500

    pipeline[1] AzureDevops migracion jdk 17 conección a ACR

--- src/main/java/hackathon/nttdata/coderpath/cursos/CursosApplication.java
+++ src/main/java/hackathon/nttdata/coderpath/cursos/CursosApplication.java
@@ -2,9 +2,9 @@ package hackathon.nttdata.coderpath.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CursosApplication {
 
--- src/main/java/hackathon/nttdata/coderpath/cursos/service/impl/KafkaConsumer.java
+++ src/main/java/hackathon/nttdata/coderpath/cursos/service/impl/KafkaConsumer.java
@@ -10,8 +10,8 @@ public class KafkaConsumer {
 
 	@KafkaListener(topics = "orders", groupId = "coder_path")
 	public void consume(String message,
-			@Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
-			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+			@Header(KafkaHeaders.KEY) String key,
+			@Header(KafkaHeaders.PARTITION) int partition,
 	        @Header(KafkaHeaders.OFFSET) int offset) {
 		System.out.println("message received: " + message);
 		System.out.println("partition: " + partition + " offset: " + offset + " key: " + key);
